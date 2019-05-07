4de49b745a205943480d1a6f815de3afe83f182f
I4PROINFO\anmak <andrew.mak@i4pro.com.br>
2019-05-07 18:43:54 -0300

    rotas com autenticação jwt

--- src/router/index.js
+++ src/router/index.js
@@ -5,26 +5,107 @@ import VueRouter from "vue-router";
 import LatestMovie from "@/components/LatestMovie";
 import Movie from "@/components/Movie";
 import SearchMovie from "@/components/SearchMovie";
+import Login from '@/components/Login';
+import Register from '@/components/Register';
+import Admin from '@/components/Admin';
+
 Vue.use(VueRouter);
-export default new VueRouter({
+
+let router = new VueRouter({
   routes: [
+    {
+      path: "*",      
+      meta: { 
+          requiresAuth: true,
+        }
+      
+    },
     {
       path: "/",
       name: "LatestMovie",
-      component: LatestMovie
+      component: LatestMovie,
+      meta: { 
+          requiresAuth: true,
+        }
+      
     },
+    {
+      path: '/login',
+      name: 'login',
+      component: Login,
+      meta: { 
+          guest: true
+      }
+  },
+  {
+      path: '/register',
+      name: 'register',
+      component: Register,
+      meta: { 
+          guest: true
+      }
+  },
     {
       path: "/movie/:id",
       name: "Movie",
       props: true,
-      component: Movie
+      component: Movie,
+      meta: { 
+          requiresAuth: true,
+        }
     },
     {
       path: "/search/:name",
       name: "SearchMovie",
       props: true,
-      component: SearchMovie
-    }
+      component: SearchMovie,
+      meta: { 
+          requiresAuth: true,
+        }
+    },
+    {
+      path: '/admin',
+      name: 'admin',
+      component: Admin,
+      meta: { 
+          requiresAuth: true,
+          is_admin : true
+      }
+  },
   ],
   mode: "history"
 });
+
+router.beforeEach((to, from, next) => {
+  if(to.matched.some(record => record.meta.requiresAuth)) {
+      if (localStorage.getItem('jwt') == null) {
+          next({
+              path: '/login',
+              params: { nextUrl: to.fullPath }
+          })
+      } else {
+          let user = JSON.parse(localStorage.getItem('user'))
+          if(to.matched.some(record => record.meta.is_admin)) {
+              if(user.is_admin == 1){
+                  next()
+              }
+              else{
+                  next({ name: 'LatestMovie'})
+              }
+          }else {
+              next()
+          }
+      }
+  } else if(to.matched.some(record => record.meta.guest)) {
+      if(localStorage.getItem('jwt') == null){
+          next()
+      }
+      else{
+          next({ name: 'LatestMovie'})
+      }
+  }else {
+      next() 
+  }
+})
+
+export default router
